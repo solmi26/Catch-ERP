9a7b6ff9831bd501eb7f5e63270108876d285100
solmi <103156290+solmi26@users.noreply.github.com>
2024-11-12 17:26:51 +0900

    타임리프 레이아웃 설정

---
+++ catch-erp/src/main/java/com/cherp/app/test/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.cherp.app.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+	
+	@GetMapping("test")
+	public String testView() {
+		return "test/test";
+	}
+}
