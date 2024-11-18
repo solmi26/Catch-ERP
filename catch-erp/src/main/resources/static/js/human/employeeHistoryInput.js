16b64c7fcacaaebf37171308b161ee2a2c893f44
seok <timeshort@naver.com>
2024-11-18 19:26:06 +0900

    인사 수정

--- catch-erp/src/main/resources/static/js/human/employeeHistoryInput.js
+++ catch-erp/src/main/resources/static/js/human/employeeHistoryInput.js
@@ -6,5 +6,28 @@
  document.querySelector('.addRowBtn').addEventListener('click',function () {
 	let standardDate =document.querySelector('.standard-date')
 	
-	grid.appendRow({name:standardDate})
+	grid.appendRow({name:standardDate.value})
+	
+
  })
+ 
+document.querySelector('.deleteBtn').addEventListener('click',  function () {
+	 grid.removeCheckedRows();
+	 
+	refreshRowNum();		
+	
+})
+
+
+function refreshRowNum () {
+	 window.setTimeout(function () {
+	let checkList = document.querySelectorAll('.countCheck')
+	console.log(checkList.length);
+	let num = 1;
+	checkList.forEach(items => {
+		items.innerText = num;
+		num += 1;
+	})
+		
+	 }, 50)
+}
