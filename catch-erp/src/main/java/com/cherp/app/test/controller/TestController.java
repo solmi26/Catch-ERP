e6e2e2abf169919d296bb99b6d4163ab0942796f
DESKTOP-105S69J\admin <rnjsdn2481@naver.com>
2024-11-18 17:48:03 +0900

    FETCH TEST (로컬 json파일 테스트 하려면 static 폴더 아래에 파일이 존재해야함)

--- catch-erp/src/main/java/com/cherp/app/test/controller/TestController.java
+++ catch-erp/src/main/java/com/cherp/app/test/controller/TestController.java
@@ -26,8 +26,15 @@ public class TestController {
 	@GetMapping("test1")
 	public String testView2() {
 		//return "account/viewBankAccount"; // 계좌목록
-		return "account/regDebtReduction"; // 채무감소
+		//return "account/regDebtReduction"; // 채무감소
 		//return "account/regBondReduction"; // 채권감소
+		return "account/contract";
+	}
+	@GetMapping("test2")
+	public String testView3() {
+		//return "account/viewBankAccount"; // 계좌목록
+		//return "account/regDebtReduction"; // 채무감소
+		return "account/regBondReduction"; // 채권감소
 		//return "account/contract";
 	}
 }
