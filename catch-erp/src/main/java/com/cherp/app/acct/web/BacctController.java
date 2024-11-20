0c9a5a76f9671960efa2d0ad3469d81058485360
DESKTOP-105S69J\admin <rnjsdn2481@naver.com>
2024-11-20 18:55:59 +0900

    계좌목록 페이지  - 계좌 생성모달, 수정모달 기능구현 끝

--- catch-erp/src/main/java/com/cherp/app/acct/web/BacctController.java
+++ catch-erp/src/main/java/com/cherp/app/acct/web/BacctController.java
@@ -1,10 +1,14 @@
 package com.cherp.app.acct.web;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.cherp.app.acct.service.BacctService;
 import com.cherp.app.acct.vo.BacctVO;
@@ -22,6 +26,21 @@ public class BacctController {
 	public String bacctList(Model model) {
 		List<BacctVO> list = bacctService.bacctList();
 		model.addAttribute("bacct",list);
+		return "account/viewBankAccount";
+	}
+	
+	// 신규 계좌 저장
+	@PostMapping("insertBacct")
+	//@ResponseBody
+	public String insertBacct(BacctVO bacctVO) {
+		bacctService.bacctInsert(bacctVO);
+		return "account/viewBankAccount";
+	}
+	
+	// 계좌 수정
+	@PostMapping("updateBacct")
+	public String updateBacct(BacctVO bacctVO) {
+		bacctService.bacctUpdate(bacctVO);
 		return "account/viewBankAccount";
 	}
 }
