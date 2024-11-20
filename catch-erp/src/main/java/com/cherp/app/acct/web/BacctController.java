b0e5a6276ebdfe6a77dc756f161f11892232f08c
DESKTOP-105S69J\admin <rnjsdn2481@naver.com>
2024-11-21 08:36:18 +0900

    계좌 수정작업 완료

--- catch-erp/src/main/java/com/cherp/app/acct/web/BacctController.java
+++ catch-erp/src/main/java/com/cherp/app/acct/web/BacctController.java
@@ -1,14 +1,11 @@
 package com.cherp.app.acct.web;
 
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.cherp.app.acct.service.BacctService;
 import com.cherp.app.acct.vo.BacctVO;
