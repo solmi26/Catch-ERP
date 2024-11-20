df2fb989b6ec02067e1f55670e5c703a7c93dd3a
DESKTOP-105S69J\admin <rnjsdn2481@naver.com>
2024-11-20 12:22:23 +0900

    순환참조 오류 해결(ServiceImpl에서 매퍼를 호출한것이 아니라 서비스를 호출해서 생긴 문제였음) BacctController 추가 , 계좌목록 html JS 구현중

--- catch-erp/src/main/java/com/cherp/app/acct/service/impl/BacctServiceImpl.java
+++ catch-erp/src/main/java/com/cherp/app/acct/service/impl/BacctServiceImpl.java
@@ -4,40 +4,41 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.cherp.app.acct.mapper.BacctMapper;
 import com.cherp.app.acct.service.BacctService;
 import com.cherp.app.acct.vo.BacctVO;
 
 @Service
 public class BacctServiceImpl implements BacctService{
-	private BacctService bacctService;
+	private BacctMapper bacctMapper;
 	
-	public BacctServiceImpl(BacctService bacctService) { // 순환 참조 회피 방법 @Lazy : 필요한 시점에만 참조를 생성
-		this.bacctService = bacctService;
+	public BacctServiceImpl(BacctMapper bacctMapper) { // 순환 참조 회피 방법 @Lazy : 필요한 시점에만 참조를 생성
+		this.bacctMapper = bacctMapper;
 	}
 	
 	@Override
 	public List<BacctVO> bacctList() {
-		return bacctService.bacctList();
+		return bacctMapper.selectAllBacctList();
 	}
 
 	@Override
 	public BacctVO bacctInfo(BacctVO bacctVO) {
-		return bacctService.bacctInfo(bacctVO);
+		return bacctMapper.selectBacct(bacctVO);
 	}
 
 	@Override
 	public int bacctInsert(BacctVO bacctVO) {
-		return bacctService.bacctInsert(bacctVO);
+		return bacctMapper.insertBacct(bacctVO);
 	}
 
 	@Override
 	public int bacctUpdate(BacctVO bacctVO) {
-		return bacctService.bacctUpdate(bacctVO);
+		return bacctMapper.updateBacct(bacctVO);
 	}
 
 	@Override
 	public int bacctDelete(int bacct) {
-		return bacctService.bacctDelete(bacct);
+		return bacctMapper.deleteBacct(bacct);
 	}
 
 }
