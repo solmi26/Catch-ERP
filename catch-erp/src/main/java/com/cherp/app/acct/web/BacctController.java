e0a4609fa5f58004f3c3f0b8e9e65ebae86a02de
DESKTOP-105S69J\admin <rnjsdn2481@naver.com>
2024-11-20 16:36:58 +0900

    계좌 관련 백앤드 작업. main에서 작업함

--- catch-erp/src/main/java/com/cherp/app/acct/web/BacctController.java
+++ catch-erp/src/main/java/com/cherp/app/acct/web/BacctController.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.cherp.app.acct.service.BacctService;
 import com.cherp.app.acct.vo.BacctVO;
@@ -22,6 +25,14 @@ public class BacctController {
 	public String bacctList(Model model) {
 		List<BacctVO> list = bacctService.bacctList();
 		model.addAttribute("bacct",list);
+		return "account/viewBankAccount";
+	}
+	
+	// 계좌 등록
+	@PostMapping("insertBacct")
+	@ResponseBody
+	public String insertBacct(BacctVO bacct) {
+		bacctService.bacctInsert(bacct);
 		return "account/viewBankAccount";
 	}
 }
