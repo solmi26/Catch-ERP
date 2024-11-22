693771b06bae9e58063d5d4d8066e70d0632ed1b
h0203t <rnjsgurxo12345@gmail.com>
2024-11-22 17:36:54 +0900

    11-22 퇴실
    판매 페이지 input 완료
    판매 그리드 추가, 삭제 완료

---
+++ catch-erp/src/main/java/com/cherp/app/buss/web/RestWarehouseController.java
@@ -0,0 +1,26 @@
+package com.cherp.app.buss.web;
+
+import com.cherp.app.buss.service.WarehouseService;
+import com.cherp.app.buss.vo.WarehouseVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class RestWarehouseController {
+
+    private WarehouseService warehouseService;
+
+    @Autowired
+    public RestWarehouseController(WarehouseService warehouseService) {
+        this.warehouseService = warehouseService;
+    }
+
+    @GetMapping("whList")
+    public List<WarehouseVO> whList(){
+        return warehouseService.whList();
+    }
+
+}
