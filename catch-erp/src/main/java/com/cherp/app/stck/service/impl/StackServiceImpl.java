645adecffd8b8dc793a69dcc02880c015fcdc722
KunWooNam <gwnam2077@gmail.com>
2024-11-22 19:26:57 +0900

    src/main/java 패키지에 stack 관련 패키지들 만듬

---
+++ catch-erp/src/main/java/com/cherp/app/stck/service/impl/StackServiceImpl.java
@@ -0,0 +1,5 @@
+package com.cherp.app.stck.service.impl;
+
+public class StackServiceImpl {
+
+}
---
+++ catch-erp/src/main/java/com/cherp/app/stck/web/StackAdjustController.java
@@ -0,0 +1,23 @@
+package com.cherp.app.stck.web;
+
+import org.springframework.stereotype.Controller;
+
+import com.cherp.app.acct.service.BacctService;
+import com.cherp.app.acct.service.SalesService;
+import com.cherp.app.buss.web.BussController;
+import com.cherp.app.stck.service.StackService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/*재고조정페이지 Controller*/
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class StackAdjustController {
+	
+	private final StackService stackAdjustService;
+	
+	
+}
