457acee63637016290117bfd47fa4ead149bfacf
kw <gwnam2077@gmail.com>
2024-11-24 21:05:35 +0900

    stackServiceImpl 삭제

--- catch-erp/src/main/java/com/cherp/app/stck/web/StockAdjustController.java
+++ catch-erp/src/main/java/com/cherp/app/stck/web/StockAdjustController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.cherp.app.buss.service.ClientService;
 import com.cherp.app.stck.service.StockService;
 
 import lombok.RequiredArgsConstructor;
@@ -21,7 +22,6 @@ public class StockAdjustController {
 
 //	private final StockService stockAdjustService;
 	private final ClientService clientServcie;
-
 	private final StockService stockAdjustService;
 	
 	@GetMapping("stocks/stockAdjustment")
--- catch-erp/src/main/java/com/cherp/app/stck/web/rest/RestStockAdjustController.java
+++ catch-erp/src/main/java/com/cherp/app/stck/web/rest/RestStockAdjustController.java
@@ -19,7 +19,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class RestStockAdjustController {
 	
-//	private final StockService stockAdjustService;
+	private final StockService stockAdjustService;
 	private final ClientService clientServcie;
 	
 	@GetMapping("stocks/clientList")
@@ -31,4 +31,6 @@ public class RestStockAdjustController {
 	public List<ContractItemVO> getItemList() {
 		return stockAdjustService.getItemList();
 	}
+	
+	
 }
