4a0a4eef5c6a78075623d7eaaa5ee2afdff64f5c
h0203t <rnjsgurxo12345@gmail.com>
2024-11-26 17:41:50 +0900

    판매 등록(창고 선택 추가)

--- catch-erp/src/main/resources/static/js/sales/salesChit.js
+++ catch-erp/src/main/resources/static/js/sales/salesChit.js
@@ -1,12 +1,7 @@
 document.addEventListener("DOMContentLoaded", async function () {
 
-    async function loadWhList() {
-        let result =  await fetch('whList');
-        result = await result.json();
-        return result;
-    }
-
-    let whList = await loadWhList();
+    // 입력일자 sysdate input
+    document.getElementById('currentDate').value = new Date().toISOString().substring(0, 10);
 
     // 계좌 목록 조회 모달
     let accountList = [{accCode: "000001", accName: "신한(입금)"}, {accCode: "000002", accName: "대구(출금)"},];
@@ -24,8 +19,6 @@ document.addEventListener("DOMContentLoaded", async function () {
         },],
     });
 
-    const myModal = new bootstrap.Modal('#accountSearchModal')
-
     accList.on("cellClick", (e, cell) => {
 
         const rowData = cell.getRow().getData(); // 클릭한 셀의 값
@@ -35,45 +28,6 @@ document.addEventListener("DOMContentLoaded", async function () {
         const modalToggle = document.getElementById('accountSearchModal');
         modalToggle.hide(modalToggle);
     });
-
-
-    class ImageRenderer {
-        constructor(props) {
-            this.el = document.createElement('img');
-            this.el.src = props.value; // 이미지 URL을 props.value로 설정
-            this.el.style.width = '130px'; // 이미지 크기 조절
-            this.el.style.height = '100px';
-        }
-
-        getElement() {
-            return this.el;
-        }
-    }
-
-    class CustomTextEditor {
-        constructor(props) {
-            const el = document.createElement('input');
-            const {maxLength} = props.columnInfo.editor.options;
-
-            el.type = 'text';
-            el.maxLength = maxLength;
-            el.value = String(props.value);
-
-            this.el = el;
-        }
-
-        getElement() {
-            return this.el;
-        }
-
-        getValue() {
-            return this.el.value;
-        }
-
-        mounted() {
-            this.el.select();
-        }
-    }
 
     class ButtonRenderer {
         constructor(props) {
@@ -87,79 +41,6 @@ document.addEventListener("DOMContentLoaded", async function () {
 
         getElement() {
             return this.el;
-        }
-    }
-
-    //숫자타입 인풋 렌더러 (석진제작)
-    class gridNumber {
-        constructor(props) {
-            const el = document.createElement('input');
-
-            el.type = 'number';
-            el.value = String(props.value);
-            el.className = 'form-control from-control-sm'
-            this.el = el;
-        }
-
-        getElement() {
-            return this.el;
-        }
-
-        getValue() {
-            return this.el.value;
-        }
-
-        mounted() {
-            this.el.select();
-        }
-    }
-
-    //숫자있는 체크박스 (석진제작)
-    class gridCheckbox {
-        constructor(props) {
-            const {grid, rowKey} = props;
-
-            const label = document.createElement('label');
-            label.className = 'checkbox tui-grid-row-header-checkbox selectCheck countCheck';
-            label.setAttribute('for', 'selectCheck' + String(rowKey));
-            label.innerText = `${grid.getIndexOfRow(rowKey) + 1}`;
-            const hiddenInput = document.createElement('input');
-            hiddenInput.className = 'hidden-input';
-            hiddenInput.id = 'selectCheck'  + String(rowKey);
-
-            console.log(grid.el.id);
-            const customInput = document.createElement('span');
-            customInput.className = 'custom-input';
-
-            customInput.appendChild(hiddenInput);
-            customInput.appendChild(label);
-
-            hiddenInput.type = 'checkbox';
-            label.addEventListener('click', (ev) => {
-                ev.preventDefault();
-
-                if (ev.shiftKey) {
-                    grid[!hiddenInput.checked ? 'checkBetween' : 'uncheckBetween'](rowKey);
-                    return;
-                }
-
-                grid[!hiddenInput.checked ? 'check' : 'uncheck'](rowKey);
-            });
-
-            this.el = customInput;
-
-            this.render(props);
-        }
-
-        getElement() {
-            return this.el;
-        }
-
-        render(props) {
-            const hiddenInput = this.el.querySelector('.hidden-input');
-            const checked = Boolean(props.value);
-
-            hiddenInput.checked = checked;
         }
     }
 
@@ -190,7 +71,6 @@ document.addEventListener("DOMContentLoaded", async function () {
     });
 
     let clientGrid; //모달에 적용될 그리드라서 refreshLayout() 사용을 위해 전역스코프로 변수를 선언하였음.
-
     const initClientGrid = () => {
 
         // 그리드 객체
@@ -267,6 +147,19 @@ document.addEventListener("DOMContentLoaded", async function () {
                 filter: 'select'
             }]
         });
+        //거래처 input
+        clientGrid.on("click", (ev) => {
+            const clientRowData = clientGrid.getRow(ev.rowKey);
+
+            if (clientRowData && clientRowData.clientName) {
+                // 특정 열(columnName)의 값 가져오기
+                // const columnValue = clientGrid.getValue(ev.rowKey, 'clientName');
+                document.getElementById('inputClientName').value = clientRowData.clientName;
+                document.getElementById('inputClientCode').value = clientRowData.clientCode;
+            }
+        });
+
+
         return clientGrid;
     }
 
@@ -274,6 +167,7 @@ document.addEventListener("DOMContentLoaded", async function () {
             StackInquery 사원조회 모달 JS
         ==============================*/
     // 모달 관련 JavaScript
+
     let humanModalBtn = document.getElementById('humanModal')
     humanModalBtn.addEventListener('show.bs.modal', function () {
         // 모달이 표시되기 전에 실행할 코드
@@ -286,15 +180,8 @@ document.addEventListener("DOMContentLoaded", async function () {
         // 모달이 완전히 숨겨진 후 실행할 코드
     })
 
-    fetch('emps')
-        .then(result => result.json())
-        .then(data => humanGrid.resetData(data))
-        .catch(error => alert("데이터를 조회하는데 실패"))
-
-    const empData = [{}];
-
     let humanGrid;
-    const inithumanGrid = () => {
+    const initHumanGrid = () => {
         humanGrid = new Grid({
             el: document.getElementById('humanGrid'),
             scrollX: true,
@@ -336,88 +223,29 @@ document.addEventListener("DOMContentLoaded", async function () {
                 filter: 'select'
             }]
         });
+        fetch('/emps')
+            .then(result => result.json())
+            .then(data => humanGrid.resetData(data))
+            .catch(error => alert("사원 데이터를 조회하는데 실패"))
+
         return humanGrid;
     }
 
-    const createdhumanGrid = inithumanGrid();
-
-    /*============================
-            StackInquery 창고 모달 JS
-        ==============================*/
-
-    // // 모달 관련 JavaScript
-    // const warehouseModal = document.getElementById('warehouseModal')
-    //
-    // //모달실행 시 grid refresh를 위한 코드
-    // document.getElementById('openWarehouseModal').addEventListener('click', function () {
-    //     window.setTimeout(function () {
-    //         warehouseGrid.refreshLayout();
-    //     }, 200)
-    // });
-    //
-    // // 샘플 데이터
-    // const warehouseData = [{}];
-    //
-    // let warehouseGrid;
-    // const initWarehouseGrid = () => {
-    //     warehouseGrid = new Grid({
-    //         el: document.getElementById('warehouseGrid'),
-    //         scrollX: true,
-    //         scrollY: true,
-    //         header: {height: 40},
-    //         bodyHeight: 500,
-    //         width: 'auto',
-    //         contextMenu: null,
-    //         rowHeaders: [{
-    //             type: 'rowNum', header: "No.", width: 50, className: 'border'
-    //         }],
-    //         columns: [{
-    //             header: '창고명',
-    //             name: 'whName',
-    //             align: "center",
-    //             width: 183,
-    //             whiteSpace: 'normal',
-    //             className: 'border',
-    //             renderer: {
-    //                 type: ButtonRenderer
-    //             },
-    //             filter: 'select'
-    //
-    //         }, {
-    //             header: '창고코드', name: 'whCode', align: "center", width: 183, whiteSpace: 'normal', className: 'border'
-    //         }, {
-    //             header: '위치',
-    //             name: 'whPlace',
-    //             align: "center",
-    //             width: 184,
-    //             whiteSpace: 'normal',
-    //             className: 'border',
-    //             filter: 'select'
-    //         }, {
-    //             header: '구분',
-    //             name: 'whType',
-    //             align: "center",
-    //             width: 184,
-    //             whiteSpace: 'normal',
-    //             className: 'border',
-    //             filter: 'select'
-    //         }]
-    //     });
-    //     return warehouseGrid;
-    // }
-    //
-    // const createdWarehouseGrid = initWarehouseGrid();
+    initHumanGrid();
 
     /*============================
             매출계정 모달 JS
     ==============================*/
+
     // 모달 관련 JavaScript
     const accCodeModal = document.getElementById('accCodeGrid')
 
-    fetch('acctList')
-        .then(result => result.json())
-        .then(data => accCodeGrid.resetData(data))
-        .catch(error => alert("데이터를 조회하는데 실패"))
+    window.setTimeout(function () {
+        fetch('/acctList')
+            .then(result => result.json())
+            .then(data => accCodeGrid.resetData(data))
+            .catch(error => alert("매출계정 데이터를 조회하는데 실패"))
+    }, 200)
 
     //모달실행 시 grid refresh를 위한 코드
     document.getElementById('openAccCodeModal').addEventListener('click', function () {
@@ -453,9 +281,14 @@ document.addEventListener("DOMContentLoaded", async function () {
              판매 등록 그리드
     ==============================*/
 
+    async function loadWhList() {
+        let result = await fetch('/whList');
+        result = await result.json();
+        result = result.map(ele => ({text: ele.whName, value: ele.whCode}));
+        return result;
+    }
 
-
-
+    let whList = await loadWhList();
 
     let salesChit;
     const initSalesChitGrid = () => {
@@ -471,13 +304,14 @@ document.addEventListener("DOMContentLoaded", async function () {
             contextMenu: null,
             rowHeaders: [{
                 type: 'checkbox', header: `
-                        <span class="custom-input">
-                            <input type="checkbox" id="all-checkbox" class="hidden-input" name="_checked" />
-                            <label for="all-checkbox" class="checkbox selectCheck">✔</label>
-                        </span>`, renderer: {
+                      <span class="custom-input">
+                          <input type="checkbox" id="all-checkbox" class="hidden-input" name="_checked" />
+                          <label for="all-checkbox" class="checkbox selectCheck">✔</label>
+                      </span>`, renderer: {
                     type: gridCheckbox
                 }
             }],
+            data: whList,
             columns: [{
                 header: '품목코드',
                 name: 'prodCode',
@@ -512,12 +346,12 @@ document.addEventListener("DOMContentLoaded", async function () {
                 formatter: 'listItemText',
                 editor: {
                     type: 'select',
-                    options:{
+                    options: {
                         listItems: whList
                     }
                 },
                 className: 'border'
-            },{
+            }, {
                 header: '재고수량',
                 name: 'stocksQuantity',
                 align: "center",
@@ -607,7 +441,7 @@ document.addEventListener("DOMContentLoaded", async function () {
         let num = 1;
         window.setTimeout(function () {
             let checkList = document.querySelectorAll('.countCheck');
-             // 번호 초기값 설정
+            // 번호 초기값 설정
             checkList.forEach(items => {
                 items.innerText = num; // 번호 부여
                 num += 1; // 번호 증가
@@ -615,9 +449,7 @@ document.addEventListener("DOMContentLoaded", async function () {
         }, 200); // 200ms 딜레이 후 실행
     }
 
-    const salesChitData = [];
-
-    const createdSalesChitGrid = initSalesChitGrid();
+    initSalesChitGrid();
 
     // 그리드 설정
     const createdClientGrid = initClientGrid();
@@ -625,60 +457,11 @@ document.addEventListener("DOMContentLoaded", async function () {
     // 그리드에 창고 데이터 넣기(출력)
     // createdWarehouseGrid.resetData(warehouseData);
 
-    createdSalesChitGrid.resetData(salesChitData);
-
     // 그리드에 데이터 넣기(출력)
     createdhumanGrid.resetData(empData);
 
     // 그리드에 데이터 넣기(출력)
     createdAccCodeGrid.resetData(accCodeData);
-
-    // 그리드에 데이터 넣기(출력)
-    // createdClientGrid.resetData(clientData);
-
-    // 그리드 클릭 시 input 입력
-
-
-    // 입력일자 sysdate input
-    document.getElementById('currentDate').value = new Date().toISOString().substring(0, 10);
-
-    //거래처 input
-    clientGrid.on("click", (ev) => {
-        const clientRowData = clientGrid.getRow(ev.rowKey);
-
-        if (clientRowData && clientRowData.clientName) {
-            // 특정 열(columnName)의 값 가져오기
-            // const columnValue = clientGrid.getValue(ev.rowKey, 'clientName');
-            document.getElementById('inputClientName').value = clientRowData.clientName;
-            document.getElementById('inputClientCode').value = clientRowData.clientCode;
-        }
-    });
-
-    //담당자 input
-    humanGrid.on("click", (ev) => {
-        const empRowData = humanGrid.getRow(ev.rowKey);
-
-        if (empRowData && empRowData.employeeCode) {
-            // 특정 열(columnName)의 값 가져오기
-            // const columnValue = humanGrid.getValue(rowKey, 'name');
-            console.log(empRowData);
-            document.getElementById('empNameInput').value = empRowData.name;
-            document.getElementById('empCodeInput').value = empRowData.employeeId;
-
-        }
-    });
-
-    //창고 input
-    // warehouseGrid.on("click", (ev) => {
-    //     const columnName = ev.columnName;
-    //
-    //     if (columnName === 'whName') {
-    //         // 특정 열(columnName)의 값 가져오기
-    //         const columnValue = warehouseGrid.getValue(ev.rowKey, 'whName');
-    //         document.getElementById('warehouse').value = columnValue
-    //
-    //     }
-    // });
 
     //매출계정 input
     accCodeGrid.on("click", (ev) => {
@@ -705,10 +488,13 @@ document.addEventListener("DOMContentLoaded", async function () {
         }, 200)
     })
 
-    fetch('ordersList')
-        .then(result => result.json())
-        .then(data => ordersGrid.resetData(data))
-        .catch(error => alert("데이터를 조회하는데 실패"))
+    window.setTimeout(function () {
+        fetch('/ordersList')
+            .then(result => result.json())
+            .then(data => ordersGrid.resetData(data))
+            .catch(error => alert("발주서 데이터를 조회하는데 실패"))
+    }, 200)
+
 
     let ordersGrid;
     const initordersGrid = () => {
@@ -720,13 +506,14 @@ document.addEventListener("DOMContentLoaded", async function () {
             bodyHeight: 500,
             width: 'auto',
             contextMenu: null,
+            data: [],
             rowHeaders: [{
                 type: 'checkbox', header: `
-		              <span class="custom-input">
-		              <input type="checkbox" id="all-checkbox-2" class="hidden-input" name="_checked" />
-		                <label for="all-checkbox-2" class="checkbox selectCheck">✔</label>
-		          	</span>
-		          `, renderer: {
+                <span class="custom-input">
+                <input type="checkbox" id="all-checkbox-2" class="hidden-input" name="_checked" />
+                  <label for="all-checkbox-2" class="checkbox selectCheck">✔</label>
+              </span>
+            `, renderer: {
                     type: gridCheckbox
                 }
             }],
@@ -825,12 +612,7 @@ document.addEventListener("DOMContentLoaded", async function () {
     }
 
     // 샘플 데이터
-    const sampleData6 = [{}];
-
-    const createdOrderGrid = initordersGrid();
-    createdOrderGrid.resetData(sampleData6);
-
-
+    initordersGrid();
 
     //발주서 모달에서 선택버튼 클릭시 페이지그리드로 데이터이동
     let orderInputBtn = document.getElementById('orderInputBtn');
@@ -887,7 +669,7 @@ document.addEventListener("DOMContentLoaded", async function () {
         let resultObj = [];
         data.forEach(ele => {
             noArr.forEach(ele2 => {
-                if (ele.clientCode == ele2) {
+                if (ele.clientCode === ele2) {
                     resultObj.push(ele);
                 }
             })
@@ -908,5 +690,7 @@ document.addEventListener("DOMContentLoaded", async function () {
         })
         return resultArr
     }
+
+    const myModal = new bootstrap.Modal('#accountSearchModal')
 });
 
