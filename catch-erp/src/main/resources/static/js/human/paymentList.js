fcd90bc6ec313ba5fc04bdfec27a283974ecfbf1
유석진 <@DESKTOP-5I2O04G>
2024-12-03 22:27:28 +0900

    급여명세표

--- catch-erp/src/main/resources/static/js/human/paymentList.js
+++ catch-erp/src/main/resources/static/js/human/paymentList.js
@@ -16,6 +16,7 @@ grid.on('click',function (ev) {
 })
 
 //급여수정내역 저장버튼 클릭시
+//#region
 document.querySelector('.update-Btn').addEventListener('click',function () {
 	let datas = extractSalModifyGrid()
 	fetch("/employees/payroll", {
@@ -29,7 +30,7 @@ document.querySelector('.update-Btn').addEventListener('click',function () {
 		datoToGrid()
 	})
 })
-
+//#endregion
 //삭제버튼클릭시
 document.querySelector('.delete-Btn').addEventListener('click',function () {
 	let checkRow = grid.getCheckedRows()
@@ -49,6 +50,144 @@ document.querySelector('.delete-Btn').addEventListener('click',function () {
 		
 	})
 })
+/**
+ *
+ *  @argument ddd
+ * 
+ * 인쇄버튼클릭시 모달띄우기
+ */
+let  ary;
+let eles;
+//#region
+document.querySelector('.printModal-Btn').addEventListener('click',function () {
+	let checkRow = grid.getCheckedRows()
+	if  (checkRow.length == 0 ) {
+		return;
+	}
+	let str = ''
+	checkRow.forEach(ele => {
+		str += ','
+		str += ele.salaryNumber
+	})
+	fetch('/employees/payroll/sel?salaryNumber='+str.substring(1))
+	.then(data => data.json())
+	.then(data => {
+		data.forEach(ele => {
+			deduItem = ["employmentInsurance","healthInsurance","incomeTax","leaveRate","localTax",""]
+			let index = ele.allowanceHistoryVO.length + 3 > 5 ? ele.allowanceHistoryVO.length + 2 : 4 
+			let arr = []
+			arr.push(`<td>연장근무</td><td>${ele.overtimeAllowance}</td>`)
+			arr.push(`<td>야간근무</td><td>${ele.nightAllowance}</td>`)
+			arr.push(`<td>주말근무</td><td>${ele.weekendAllowance}</td>`)
+			
+			ele.allowanceHistoryVO.forEach(comp => {
+				arr.push(`<td>${comp.allowanceName}</td><td>${comp.allowancePrice}</td>`)			
+			})
+			ary = arr
+			console.log(arr)
+			
+			printBody = ``
+			html = 
+					`<div id="print-content">
+					  <h1>급여명세서</h1>
+					  <div>
+					  <span>${ele.payrollYdate}</span>
+					  <span></span>
+					  </div>
+					  <br>
+					  <table>
+					      <tr>
+					        <td class="background-payment">성명</td>
+					        <td colspan="3">${ele.name}</td>
+					        <td class="background-payment">사번</td>
+					        <td>${ele.employeeId}</td>
+					      </tr>
+					      <tr>
+					        <td class="background-payment">직급/직급</td>
+					        <td colspan="3"></td>
+					        <td class="background-payment">부서</td>
+					        <td></td>
+					      </tr>
+					      <tr>
+					        <td class="background-payment">생년월일</td>
+					        <td colspan="3"></td>
+					        <td class="background-payment">지급일</td>
+					        <td>${ele.payrollYdate}</td>
+					      </tr>
+					  </table>
+					<br>
+					<br>
+					<table>
+					  <thead>
+					    <tr>
+					      <td colspan="2" class="bold-color">임금지급내역</td>
+					      <td colspan="2" class="bold-color">공제내역</td>
+					    </tr>
+					    <tr>
+					      <td class="background-payment">임금항목</td>
+					      <td class="background-payment">지급액원(원)</td>
+					      <td class="background-payment">공제항목</td>
+					      <td class="background-payment">지급액원(원)</td>
+					    </tr>
+					  </thead>
+					  <tbody>
+					    <tr>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+					    </tr>
+					    <tr>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+					    </tr>					  </tbody>
+					  <tfoot>
+					    <tr>
+					      <td class="background-payment">총지급액</td>
+					      <td></td>
+					      <td class="background-payment">총공제액</td>
+					      <td></td>
+					    </tr>
+					    <tr>
+					      <td class="background-payment">실지급액</td>
+					      <td colspan="3"></td>
+					    </tr>
+					  </tfoot>
+					</table> 
+					  <br>
+					  <table>
+					    <tr>
+					      <td colspan="3" class="bold-color">계산방법</td>
+					    </tr>
+					    <tr>
+					      <td class="background-payment">구분</td>
+					      <td class="background-payment">산출식 또는 산출방법</td>
+					      <td class="background-payment">지급액(원)</td>
+					    </tr>
+					    <tr>
+					      <td class="background-payment">연장근로수당</td>
+					      <td></td>
+					      <td></td>
+					    </tr>
+					    <tr>
+					      <td class="background-payment">야간근로수당</td>
+					      <td></td>
+					      <td></td>
+					    </tr>
+					    <tr>
+					      <td class="background-payment">주말근로수당</td>
+					      <td></td>
+					      <td></td>
+					    </tr>
+					  </table>
+					</div>`
+		})
+		
+	})
+})
+//#endregion
 
 //메인그리드 데이터 불러오기
 function datoToGrid() {
