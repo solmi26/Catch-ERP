6221c9fdae2d8fd22ce3073e9dcd851d0374a951
KuwnWooNam <gwnam2077@gmail.com>
2024-12-07 17:25:28 +0900

    마이페이지 근태조회 작업중

--- catch-erp/src/main/java/com/cherp/app/common/myPage/service/MyPageService.java
+++ catch-erp/src/main/java/com/cherp/app/common/myPage/service/MyPageService.java
@@ -1,9 +1,14 @@
 package com.cherp.app.common.myPage.service;
 
+import java.util.List;
+
+import com.cherp.app.common.myPage.vo.AttendanceSearchVO;
 import com.cherp.app.common.myPage.vo.ModifiedInfoVO;
+import com.cherp.app.empl.vo.AttendanceVO;
 import com.cherp.app.empl.vo.EmployeeVO;
 
 public interface MyPageService {
 	public void modifyEmployeeInfo(ModifiedInfoVO modifiedInfoVO);  //사원정보수정
 	public EmployeeVO getEmployeeImage(String employeeCode); //변경된 사진정보 불러오기
+	public List<AttendanceVO> getAttendance(AttendanceSearchVO searchVO); //근태정보 조회
 }
--- catch-erp/src/main/java/com/cherp/app/common/myPage/service/MyPageServiceImpl.java
+++ catch-erp/src/main/java/com/cherp/app/common/myPage/service/MyPageServiceImpl.java
@@ -1,9 +1,15 @@
 package com.cherp.app.common.myPage.service;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.cherp.app.common.myPage.mapper.MyPageMapper;
+import com.cherp.app.common.myPage.vo.AttendanceSearchVO;
 import com.cherp.app.common.myPage.vo.ModifiedInfoVO;
+import com.cherp.app.empl.vo.AttendanceVO;
 import com.cherp.app.empl.vo.EmployeeVO;
 import com.cherp.app.stck.web.StocksController;
 
@@ -19,14 +25,36 @@ public class MyPageServiceImpl implements MyPageService {
 		this.myPageMapper = myPageMapper;
 	}
 	
+	//마이페이지 내 정보수정
 	@Override
 	public void modifyEmployeeInfo(ModifiedInfoVO modifiedInfoVO) {
 		myPageMapper.updateEmployeeInfo(modifiedInfoVO);
 	}
 
+	//본인 사진조회
 	@Override
 	public EmployeeVO getEmployeeImage(String employeeCode) {
 		return myPageMapper.selectEmployeeImage(employeeCode);
+	}
+
+	//근태정보조회
+	@Override
+	public List<AttendanceVO> getAttendance(AttendanceSearchVO searchVO) {
+		AttendanceSearchVO filteredSearchVO = new AttendanceSearchVO();
+		if(searchVO.getYearCondition() == null) {
+			SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy");
+			SimpleDateFormat monthFormat = new SimpleDateFormat("MM");
+			Date now = new Date();         
+			String year = yearFormat.format(now);
+			String month = monthFormat.format(now);
+			filteredSearchVO.setEmployeeCode(searchVO.getEmployeeCode());
+			filteredSearchVO.setYearCondition(year);
+			filteredSearchVO.setMonthCondition(month);
+			System.out.println("체크합니다." + year + "년 / 달" + month); //-> 2024년 / 달12 정상
+			return myPageMapper.selectAttendance(filteredSearchVO); //페이지 최초로드시 최신 년도와 달의 근태조회
+		} else {
+			return myPageMapper.selectAttendance(searchVO); //수동조회 시 입력받은 값에따른 근태조회
+		}
 	}
 
 }
